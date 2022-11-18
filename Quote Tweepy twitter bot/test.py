154f268781c0b336da9c95b80f623894f1634ed2
omnix <96026994+hoo12F@users.noreply.github.com>
2022-11-17 21:54:37 -0800

    Delete test.py

--- Quote Tweepy twitter bot/test.py
+++
@@ -1,7 +0,0 @@
-def test_quote_1():
-    search = "Bitcoin Billionaires"
-    result = quote(search, limit=5)
-    one = result[0]
-    assert (
-        one["author"] == "Ben Mezrich" and one["book"] and isinstance(one["quote"], str)
-    )
