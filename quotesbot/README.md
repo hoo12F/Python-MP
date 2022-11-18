00f8557a342118d7608e59a9568f6d15a1e4b9a8
omnix <96026994+hoo12F@users.noreply.github.com>
2022-11-18 15:41:51 -0800

    Add quotes

---
+++ quotesbot/README.md
@@ -0,0 +1,2 @@
+# TODO
+1. actually turn this into a discord bot (change of plans)
---
+++ quotesbot/main.py
@@ -0,0 +1,37 @@
+import discord
+import requests
+from datetime import datetime
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Pong')
+    if message.content.startswith('$quote'):
+        get = requests.get(
+            'https://quote-garden.herokuapp.com/api/v3/quotes/random')
+        if get.status_code == 200:
+            jsonStuff = get.json()
+            data = jsonStuff['data']
+
+            await message.channel.send(data[0]['quoteText'])
+
+        else:
+            await message.author.send(f"There was an error fetching the quote. The http code is {get.status_code}")
+    if message.content.startswith('$error'):
+        await message.author.send(f"There was an error fetching the quote.")
+
+client.run(
+    'MTA0MzE5ODY2MjQ0NjE2MTk5MA.GSn-sp.ZE_m64YTdLMp4hsgz_-l9Inf43J_FWl82YPB5E')
